7a501f3a79215d124fe77a4399ef31af27a0bef7
taimoorkiani <taimoorkiani602@gmail.com>
2026-03-30 14:53:41 -0600

    finish write_source and start compile methods

--- src/executor.h
+++ src/executor.h
@@ -1,6 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include "job.h"
+#include <format>
+#include <unistd.h>
+#include <sys/wait.h>
 
 
-#ifndef NEXEC_EXECUTOR_H
-#define NEXEC_EXECUTOR_H
+class Executor {
 
-#endif
+public:
+    std::string write_source(const Job& job) {
+        std::string filename = "/tmp/nexec_" + std::to_string(job.id) + ".cpp";
+        std::ofstream code_file(filename);
+        code_file << job.source_code << std::endl;
+        code_file.close();
+        return filename;
+    }
+
+    std::string compile(const std::string& source_path, int job_id) {
+        char* args[] = {(char*)"executor.h", NULL};
+        const char* command = "g++";
+
+        pid_t c_pid = fork();
+        if (c_pid == -1) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+        else if (c_pid > 0) {
+            std::cout << "print from parent" << getpid() << std::endl;
+        }
+        else {
+            std::cout << "print from child" << getpid() << std::endl;
+        }
+        return 0;
+    }
+
+};
