a0c4487ae114b61c62a239fbd4e2c4df622495ae
taimoorkiani <taimoorkiani602@gmail.com>
2026-03-31 13:43:14 -0600

    add waitpid and finish compile function

--- src/executor.h
+++ src/executor.h
@@ -18,21 +18,42 @@ public:
     }
 
     std::string compile(const std::string& source_path, int job_id) {
-        char* args[] = {(char*)"executor.h", NULL};
+        std::string binary_path = "/tmp/nexec_" + std::to_string(job_id);
+
+        char* args[] = {
+            (char*)"g++",
+            (char*)"-o",
+            (char*)binary_path.c_str(),
+            (char*)source_path.c_str(),
+            NULL
+        };
         const char* command = "g++";
 
         pid_t c_pid = fork();
+
         if (c_pid == -1) {
-            perror("fork");
+            perror("fork failed");
             exit(EXIT_FAILURE);
         }
-        else if (c_pid > 0) {
-            std::cout << "print from parent" << getpid() << std::endl;
+         if (c_pid == 0) {
+            int status_code = execvp(command, args);
+
+            std::cerr << "execvp() failed: " << strerror(errno) << std::endl;
+            _exit(1);
         }
         else {
-            std::cout << "print from child" << getpid() << std::endl;
+            int status;
+            pid_t result = waitpid(c_pid, &status, 0);
+
+            if (result == -1) {
+                throw std::system_error(errno, std::generic_category(), "waitpid failed");
+            }
+
+            if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
+                throw std::runtime_error("Compilation failed");
+            }
         }
-        return 0;
+        return binary_path;
     }
 
 };
