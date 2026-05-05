cab2dcbf1e838ae4d737fb7dc4ca8d7725a09864
taimoorkiani <taimoorkiani602@gmail.com>
2026-05-04 23:57:16 -0600

    create ThreadPool helper functions

--- src/thread_pool.h
+++ src/thread_pool.h
@@ -11,21 +11,59 @@ private:
     std::vector<std::thread> threads;
     std::mutex mutex;
     std::condition_variable cv;
-    bool running;
-    JobManager job_manager;
-    Executor executor;
+    bool running = false;
+    JobManager& job_manager;
+    Executor& executor;
+
+    void worker_loop() {
+        while (true) {
+            std::unique_lock<std::mutex> lock(mutex);
+            cv.wait(lock, [this] { return !job_ids.empty() || !running; });
+            if (!running && job_ids.empty()) {
+                break;
+            }
+            int job_id = job_ids.front();
+            job_ids.pop();
+
+            lock.unlock();
+
+            Job& job = job_manager.get(job_id);
+            std::string source_path = executor.write_source(job);
+            std::string binary_path = executor.compile(source_path, job_id);
+            RunResult result = executor.run(binary_path, job_id);
+
+            job.output = result.output;
+            job.error = result.error;
+            job.exit_code = result.exit_code;
+            job_manager.set_status(job_id, JobStatus::Done);
+        }
+    }
+
 
 public:
+    ThreadPool(JobManager& job_manager, Executor& executor) :
+    job_manager(job_manager),
+    executor(executor) {}
+
+
     void submit(int job_id) {
+        std::lock_guard<std::mutex> lock(mutex);
         job_ids.push(job_id);
         cv.notify_one();
     }
 
-    int start() {
-
+    void start() {
+        running = true;
+        for (int i = 0; i < 4; i++) {
+            threads.emplace_back(&ThreadPool::worker_loop, this);
+        }
     }
 
-
-
-
-}
+    void stop() {
+        running = false;
+        cv.notify_all();
+        for (std::thread& thread : threads) {
+            thread.join();
+        }
+    }
+};
