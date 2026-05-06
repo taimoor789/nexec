03794a210f1bc8c9c96392bbf4a1b7d9ce2f132a
taimoorkiani <taimoorkiani602@gmail.com>
2026-05-06 17:29:09 -0600

    add mutex

--- src/job_manager.h
+++ src/job_manager.h
@@ -1,18 +1,19 @@
-#pragma once
-#include <vector>
-#include "job.h"
+#include <mutex>
 
 class JobManager {
 private:
     std::vector<Job> jobs;
     int next_id;
+    std::mutex mtx;
 
 public:
     JobManager() {
         next_id = 1;
+        jobs.reserve(100);
     }
 
     int submit(const std::string& code, const std::string& language) {
+        std::lock_guard<std::mutex> lock(mtx);
         Job j = Job(next_id, code, language);
         jobs.push_back(j);
         next_id++;
@@ -20,21 +21,24 @@ public:
     }
 
     Job& get(int id) {
-       for (Job& job : jobs) {
-           if (job.id == id) {
-               return job;
-           }
-       }
+        std::lock_guard<std::mutex> lock(mtx);
+        for (Job& job : jobs) {
+            if (job.id == id) return job;
+        }
         throw std::runtime_error("No Job with that ID exists");
     }
 
     void set_status(int id, JobStatus new_status) {
-        Job& job = get(id);
-        job.status = new_status;
+        std::lock_guard<std::mutex> lock(mtx);
+        for (Job& job : jobs) {
+            if (job.id == id) {
+                job.status = new_status;
+                return;
+            }
+        }
     }
 
     const std::vector<Job>& all() const {
         return jobs;
     }
-
 };
